3f981bcec6a1631ca99522478038aa54cb72a51c
unwhisper <591856607@qq.com>
2021-07-28 14:24:02 +0800

    change

--- example/extension/extension.sh
+++ example/extension/extension.sh
@@ -2,8 +2,16 @@
 
 mkdir /home/extension
 
-curl -Lk https://github.com/swoole/swoole-src/archive/v4.4.14.tar.gz | gunzip | tar x -C /home/extension && \
-cd /home/extension/swoole-src-4.4.14 && \
+# swoole
+# curl -Lk https://github.com/swoole/swoole-src/archive/v4.4.14.tar.gz | gunzip | tar x -C /home/extension && \
+# cd /home/extension/swoole-src-4.4.14 && \
+# /usr/local/php/bin/phpize && \
+# ./configure --with-php-config=/usr/local/php/bin/php-config && \
+# make && make install
+
+#redis
+curl -Lk https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.tar.gz | gunzip | tar x -C /home/extension && \
+cd /home/extension/phpredis-5.3.4 && \
 /usr/local/php/bin/phpize && \
 ./configure --with-php-config=/usr/local/php/bin/php-config && \
 make && make install
